6c7271650fe62d5709a95caffb79d7fd4d40f7f5
Nurhanifa Fadhyla Lubis <nurhanifafadhylalubis@gmail.com>
2021-04-06 14:49:31 +0700

    initial code

---
+++ .vscode/pengulangan.py
@@ -0,0 +1,13 @@
+#pengulangan adalah perintah untuk mengulang suatu pernyataan tertentu
+#ada dua perintah yang dapat kita gunakan: for loops dan while loops
+num = [1, 2, 3, 4, 5]
+for a in num:
+    print ("saya dapat nilai A")
+for _ in num:
+    print ("saya mau makan Udang")
+
+#while = selama, selama satu kondisi benar maka akan tetap diulang
+x = 0
+while x < 5:
+    print (f"nilai x saat ini adalah {x}")
+    x=x+1
